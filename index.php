9cb72c9db99a44cb6f3968ba26714c9fd8b7e675
marwa1994 <marwasalem08@gmail.com>
2017-08-16 14:49:12 +0100

    save and get data from db

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php 
+
+// FILE USED TO CONNECT TO DB
+require("config.php");
+$dbconn=mysqli_connect($host,$username,$password,$db_name);
+
+
+   if(isset($_POST['action'])){
+       
+ $content=mysqli_real_escape_string($dbconn,$_POST['content']);
+       //UPDATE DATA WITH CONTENT FROM AJAX REQUEST
+
+   $sql= "UPDATE `page` SET `donnepage`='".$content."' ";
+
+   $sqlc=  mysqli_query($dbconn,$sql);
+   
+   }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -10,7 +29,7 @@
         <link rel="stylesheet" type="text/css" href="css/style.css" />
         <link rel="stylesheet" type="text/css" href="dist/css/keditor-components-1.1.5.min.css" />
         <!-- End of KEditor styles -->
-        <link rel="stylesheet" type="text/css" href="css/examples.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
         <script type="text/javascript" src="plugins/jquery-1.11.3/jquery-1.11.3.min.js"></script>
         <script type="text/javascript" src="plugins/bootstrap-3.3.6/js/bootstrap.min.js"></script>
         <script type="text/javascript">
@@ -24,6 +43,7 @@
             $.fn.tooltip = bsTooltip;
             $.fn.button = bsButton;
         </script>
+        
         <script type="text/javascript" src="plugins/jquery.nicescroll-3.6.6/jquery.nicescroll.min.js"></script>
         <script type="text/javascript" src="plugins/ckeditor-4.5.6/ckeditor.js"></script>
         <script type="text/javascript" src="plugins/ckeditor-4.5.6/adapters/jquery.js"></script>
@@ -34,12 +54,61 @@
     </head>
 
     <body>
-        <div id="content-area"></div>
+       
+       <div class="btn btn-lg btn-info" id="save">save</div>
+
+        <div id="content-area">
+            
+            
+             <?php 
+        
+        $sql1="SELECT donnepage FROM page ";
+         $query1=mysqli_query($dbconn,$sql1);
+        $row=mysqli_fetch_assoc($query1);
+        echo $row['donnepage'];
+     
+        
+        
+        ?>
+        </div>
+        
+        
+       
 
         <script type="text/javascript">
-            $(function() {
-                $('#content-area').keditor();
+            
+            
+            $(function(){
+              $('#content-area').keditor(); 
+              $("#save").click(function(){
+                //post data to database
+               $.ajax({
+                   type:'post',
+                   data:{  action:"send-data", 
+                         //GET DATA FROM KEDITOR
+                           content:$('#content-area').keditor('getContent')}
+                      
+                   
+                         
+                        ,
+                   success:function(data){
+                        
+                   console.log(data);
+                        },
+                    error:function(data){
+                       
+                       console.log(data);
+                   }
+                   
+                   
+                   
+               }) ;
+            });   
             });
+            
+            
+    
+           
         </script>
     </body>
 </html>
